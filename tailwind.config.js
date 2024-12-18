65e0eacb2ffd55d9d51c8ab085c0308008649003
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-18 23:30:53 +0900

    style: 사용자 정의 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      perspective: {
+        800: "800px",
+      },
+      rotate: {
+        "x-0": "rotateX(0deg)",
+        "x-90": "rotateX(90deg)",
+        "x-180": "rotateX(180deg)",
+      },
+      translate: {
+        "y-full": "translateY(100%)",
+        "y-0": "translateY(0)",
+      },
+      transformStyle: {
+        "preserve-3d": "preserve-3d",
+      },
+      backfaceVisibility: {
+        hidden: "hidden",
+      },
+    },
   },
   plugins: [],
 };
