de436e65fd4c71724bf76ebaaec6ff402fc9309d
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-20 01:07:03 +0900

    feat: 회원가입 페이지 추가

---
+++ src/routes/signup.lazy.tsx
@@ -0,0 +1,29 @@
+import Button from "@/components/common/button";
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
+import React from "react";
+
+export const Route = createLazyFileRoute("/signup")({
+  component: SignupComponent,
+});
+function SignupComponent() {
+  const navigate = useNavigate();
+  return (
+    <React.Fragment>
+      <p className="mb-11 text-2xl">새로운 문자 만들기</p>
+      <div className="mb-11 flex flex-col gap-5">
+        <div className="flex flex-col">
+          <p>나만의 번호</p>
+          <input type="number" className="border-b-2 border-black text-xl" />
+        </div>
+        <div className="flex flex-col">
+          <p>비밀번호</p>
+          <input type="password" className="border-b-2 border-black text-xl" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Button>만들기</Button>
+        <Button onClick={() => navigate({ to: "/login" })}>돌아가기</Button>
+      </div>
+    </React.Fragment>
+  );
+}
