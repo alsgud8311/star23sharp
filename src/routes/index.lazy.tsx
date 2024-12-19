b1e77000dfd1fe932d65e78cd4943905d8d970ba
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-20 01:06:13 +0900

    feat : 메인 화면 추가

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,9 +1,30 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
+import postbox from "@assets/postbox.png";
+import Button from "@/components/common/button";
+import React from "react";
 
-export const Route = createLazyFileRoute('/')({
+export const Route = createLazyFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  const navigate = useNavigate();
+
+  return (
+    <React.Fragment>
+      <img
+        src={postbox}
+        alt="별이삼샵"
+        className="transform translate-y-10 opacity-0 w-1/2 animate-slide-up"
+      />
+      <p className="p-4 text-xl">별이삼샵</p>
+      <Button
+        onClick={() => {
+          navigate({ to: "/login" });
+        }}
+      >
+        시작하기
+      </Button>
+    </React.Fragment>
+  );
 }
