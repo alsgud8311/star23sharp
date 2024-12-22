91f95be84382745535edd502df37a34a8e6baaa7
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-22 22:25:33 +0900

    chore : 필요 없는 route 삭제 및 signin -> create로 변경

--- src/routes/create.lazy.tsx
+++ src/routes/create.lazy.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/common/button";
 import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
 import React from "react";
 
-export const Route = createLazyFileRoute("/login")({
+export const Route = createLazyFileRoute("/create")({
   component: LoginComponent,
 });
 
@@ -10,11 +10,11 @@ function LoginComponent() {
   const navigate = useNavigate();
   return (
     <React.Fragment>
-      <p className="mb-11 text-2xl">로그인</p>
+      <p className="slideUp mb-11 text-2xl">새로운 번호 만들기</p>
       <div className="mb-11 flex flex-col gap-5">
         <div className="flex flex-col">
-          <p>나만의 번호</p>
-          <input type="number" className="border-b-2 border-black text-xl" />
+          <p>제목</p>
+          <input type="text" className="border-b-2 border-black text-xl" />
         </div>
         <div className="flex flex-col">
           <p>비밀번호</p>
@@ -22,8 +22,8 @@ function LoginComponent() {
         </div>
       </div>
       <div className="flex flex-col gap-4">
-        <Button>로그인</Button>
-        <Button onClick={() => navigate({ to: "/signup" })}>새로 만들기</Button>
+        <Button>만들기</Button>
+        {/* <Button onClick={() => navigate({ to: "/signup" })}>새로 만들기</Button> */}
       </div>
     </React.Fragment>
   );
