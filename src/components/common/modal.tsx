6deb37cba71c0254e5547367bc8c45b264bba825
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-22 22:29:42 +0900

    feat : Modal 공통 컴포넌트 추가

---
+++ src/components/common/modal.tsx
@@ -0,0 +1,31 @@
+import { ModalComponent } from "@/hooks/useModal";
+import { ReactNode } from "react";
+
+export default function Modal({
+  modal,
+  closeModal,
+  children,
+}: {
+  children: ReactNode;
+} & ModalComponent) {
+  return (
+    <>
+      {modal && (
+        <>
+          <div className="absolute left-0 top-0 h-full w-full bg-black opacity-40" />
+          <div
+            className="absolute left-0 top-0 flex h-full w-full items-center justify-center"
+            onClick={closeModal}
+          >
+            <div
+              className="z-50 flex w-3/4 items-center justify-center rounded-md bg-white p-4"
+              onClick={(e) => e.stopPropagation()}
+            >
+              {children}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
