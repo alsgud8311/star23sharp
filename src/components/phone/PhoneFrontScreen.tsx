347bc5f79925083bb2d6a796dcfb5ea8b38eb950
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-22 22:26:36 +0900

    feat : 핸드폰 화면 속 ErrorBoundary와 Suspense 설정

--- src/components/phone/PhoneFrontScreen.tsx
+++ src/components/phone/PhoneFrontScreen.tsx
@@ -1,8 +1,9 @@
 import Time from "@/components/common/time";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { GiNetworkBars } from "react-icons/gi";
 import { TbBattery4 } from "react-icons/tb";
 import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "@/components/common/errorFallback";
 
 export default function PhoneFrontScreen({
   open,
@@ -15,14 +16,18 @@ export default function PhoneFrontScreen({
 }) {
   return (
     <div
-      className={`absolute -top-full bottom-0 flex h-full w-full origin-bottom flex-col rounded-md border-2 border-black bg-slate-100 transition-transform duration-1000 ${open ? "[transform:rotateX(0deg)]" : "[transform:rotateX(180deg)]"} transform-style-preserve-3d backface-hidden flex flex-col items-center p-2`}
+      className={`absolute -top-full bottom-0 flex h-full w-full origin-bottom flex-col rounded-md border-2 border-black bg-slate-100 transition-transform duration-1000 ${
+        open ? "[transform:rotateX(0deg)]" : "[transform:rotateX(180deg)]"
+      } transform-style-preserve-3d backface-hidden flex flex-col items-center p-2`}
       onClick={switchOpen}
     >
-      <div className="my-10 h-2 w-1/4 rounded-full bg-black"></div>
-      <div className="itmes-center flex h-full w-full flex-grow flex-col justify-center rounded-2xl bg-black p-4">
+      <div className="flex h-11 w-full items-center justify-center">
+        <div className="flex h-2 w-1/4 items-center justify-center rounded-full bg-black"></div>
+      </div>
+      <div className="flex h-full w-full flex-1 flex-col justify-center rounded-2xl bg-black p-4">
         <p className="text-md p-2 text-center text-slate-300">SHOW</p>
         <div
-          className="flex h-4/5 w-full flex-col bg-slate-700 font-dotted text-white"
+          className="flex h-full w-full flex-1 flex-col bg-slate-700 font-dotted text-white"
           onClick={(e) => e.stopPropagation()}
         >
           <header className="flex h-7 w-full items-center justify-between bg-white px-2">
@@ -32,11 +37,13 @@ export default function PhoneFrontScreen({
               <Time />
             </span>
           </header>
-          <div className="flex-grow">
-            <ErrorBoundary fallback={<p>opps! something went wrong</p>}>
-              <div className="flex h-full w-full flex-col items-center justify-center bg-white text-black">
-                {children}
-              </div>
+          <div className="w-full flex-1">
+            <ErrorBoundary fallback={<ErrorFallback />}>
+              <Suspense>
+                <div className="relative flex h-full w-full flex-col items-center justify-center bg-white text-black">
+                  {children}
+                </div>
+              </Suspense>
             </ErrorBoundary>
           </div>
         </div>
