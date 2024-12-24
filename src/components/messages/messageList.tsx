86fe3fae5ce0da9a2b5dd909607548e49cce2bed
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 01:16:01 +0900

    feat : MessageList 컴포넌트 분리 및 무한스크롤 추가

--- src/components/messages/messageList.tsx
+++ src/components/messages/messageList.tsx
@@ -1,9 +1,23 @@
 import NoMessage from "@/components/messages/noMessage";
+import useMessageList from "@/hooks/useMessageList";
 import useModal from "@/hooks/useModal";
+import useObserver from "@/hooks/useObserver";
 import { Message } from "@/types/message";
+import { useNavigate } from "@tanstack/react-router";
+import { useRef } from "react";
 
-export default function MessageList({ messages }: { messages: Message[] }) {
+export default function MessageList() {
   const { modal: linkmodal, openModal, closeModal } = useModal();
+  const { data: messages, fetchNextPage, hasNextPage } = useMessageList();
+  const bottomRef = useRef(null);
+  const onIntersect = ([entry]: IntersectionObserverEntry[]) =>
+    entry.isIntersecting && hasNextPage && fetchNextPage();
+  const navigate = useNavigate();
+
+  useObserver<Message[]>({
+    target: bottomRef,
+    onIntersect,
+  });
 
   if (!messages.length)
     return (
@@ -13,18 +27,29 @@ export default function MessageList({ messages }: { messages: Message[] }) {
         closeModal={closeModal}
       />
     );
+
   return (
-    <div className="grid w-full flex-grow grid-rows-8 overflow-hidden">
-      {messages.map((message: Message, index: number) => (
-        <div
-          className={`flex items-center gap-4 border-black p-2 ${index === 10 ? "" : "border-b"}`}
-        >
-          <span className="w-3">{index + 1}</span>
-          <span className="w-3/4 overflow-hidden text-ellipsis whitespace-nowrap">
-            {message.message}
-          </span>
-        </div>
-      ))}
+    <div className="relative h-full w-full overflow-hidden">
+      <div className="absolute inset-0 overflow-y-auto">
+        {messages.map((message: Message, index: number) => (
+          <div
+            key={index}
+            className="flex h-14 w-full items-center gap-4 border-b border-black p-2"
+            onClick={() =>
+              navigate({
+                to: "/messages/$messageId",
+                params: { messageId: message.id.toString() },
+              })
+            }
+          >
+            <span className="w-3">{index + 1}</span>
+            <span className="w-full overflow-hidden text-ellipsis whitespace-nowrap">
+              {message.body}
+            </span>
+          </div>
+        ))}
+        <div ref={bottomRef}></div>
+      </div>
     </div>
   );
 }
