87e5ce77000ebf73dbc2e531e8339ceec7916f27
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 01:20:25 +0900

    feat : 에러와 유효성 검증 관련 함수 추가

---
+++ src/types/error.ts
@@ -0,0 +1,4 @@
+export type Validation = {
+  result: boolean;
+  errorMsg?: string;
+};
---
+++ src/utils/validation.ts
@@ -0,0 +1,30 @@
+import { Validation } from "@/types/error";
+
+export function checkPasswordValidation(password: string) {
+  if (password.length > 4) return false;
+  return true;
+}
+
+export function passwordSubmitValidation(password: string): Validation {
+  if (/^\d{4}$/.test(password)) {
+    return {
+      result: true,
+    };
+  }
+  return {
+    result: false,
+    errorMsg: "비밀번호는 4자리 숫자여야 해요",
+  };
+}
+
+export function titleSubmitValidation(title: string): Validation {
+  if (title.length > 0) {
+    return {
+      result: true,
+    };
+  }
+  return {
+    result: false,
+    errorMsg: "제목을 입력해주세요",
+  };
+}
