f49a474deccbca0cd6afa0b17b6c9e0e64204e2a
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 01:12:20 +0900

    feat : 로그인 유지 관련 인터셉터 설정 및 axios 인스턴스 분리

---
+++ src/api/auth.api.ts
@@ -0,0 +1,49 @@
+import { instance } from "@/api";
+import logOnDev from "@/api/logging";
+import axios, { InternalAxiosRequestConfig } from "axios";
+
+export type RoomSignInRequest = {
+  room_signature: string;
+  password: string;
+};
+
+export type RoomSignInResponse = {
+  access_token: string;
+};
+
+export async function roomSignIn({
+  room_signature,
+  password,
+}: RoomSignInRequest): Promise<RoomSignInResponse> {
+  const { data } = await instance.post("/room/signin", {
+    room_signature,
+    password,
+  });
+  return data;
+}
+
+const refreshInstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_SERVER_BASE_URL,
+  withCredentials: true,
+  timeout: 3000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+// 요청 인터셉터
+refreshInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const { method, baseURL, url } = config;
+    logOnDev(`♻️ [Refresh Request] ${method?.toUpperCase()} ${baseURL} ${url}`);
+    return config;
+  },
+);
+
+type TokenRefreshResponse = {
+  access_token: string;
+};
+
+export async function tokenRefresh(): Promise<TokenRefreshResponse> {
+  const { data } = await refreshInstance.get("/room/token/reissue");
+  return data;
+}
