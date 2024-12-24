ab9ad3d1c79c9d0d4197bf8c0e904610113cab56
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 02:24:39 +0900

    refactor : modal과 pushModal 구분

--- src/components/common/pushModal.tsx
+++ src/components/common/pushModal.tsx
@@ -1,7 +1,7 @@
 import { ModalComponent } from "@/hooks/useModal";
 import { ReactNode } from "react";
 
-export default function Modal({
+export default function PushModal({
   modal,
   closeModal,
   children,
@@ -12,13 +12,13 @@ export default function Modal({
     <>
       {modal && (
         <>
-          <div className="absolute left-0 top-0 h-full w-full bg-black opacity-40" />
+          <div className="absolute left-0 top-0 z-40 h-full w-full bg-black opacity-30" />
           <div
-            className="absolute left-0 top-0 flex h-full w-full items-center justify-center"
+            className="absolute left-0 top-0 z-40 flex h-full w-full items-center justify-center"
             onClick={closeModal}
           >
             <div
-              className="z-30 flex w-3/4 translate-y-10 transform animate-slide-up items-center justify-center rounded-md bg-white p-4 opacity-0"
+              className="z-50 flex w-3/4 translate-y-10 transform animate-slide-up items-center justify-center rounded-md bg-white p-4 opacity-0"
               onClick={(e) => e.stopPropagation()}
             >
               {children}
