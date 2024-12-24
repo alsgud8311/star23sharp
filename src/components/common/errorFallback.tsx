fb878a4af1c9661a8c9d33c05a2f6eff7a839681
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 01:14:39 +0900

    feat : error boundary에 잡히는 fallback ui 추가

--- src/components/common/errorFallback.tsx
+++ src/components/common/errorFallback.tsx
@@ -1,15 +1,13 @@
 import Button from "@/components/common/button";
 import oops from "@assets/oops.png";
-import { useNavigate } from "@tanstack/react-router";
 
 export default function ErrorFallback() {
-  const navigate = useNavigate();
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-10 bg-white text-black">
       <img src={oops} className="w-1/3" alt="oops" />
       <div className="flex flex-col gap-3">
         <p>에러가 발생했어요!</p>
-        <Button onClick={() => navigate({ to: ".." })}>홈으로</Button>
+        <Button onClick={() => (location.href = "/")}>홈으로</Button>
       </div>
     </div>
   );
