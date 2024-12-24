b1854a9eb585586de70375f5cac3809a305875f2
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 01:24:04 +0900

    feat : 유효성 검증 관련 커스텀 훅 추가

---
+++ src/hooks/useValidationCheck.ts
@@ -0,0 +1,15 @@
+import { Validation } from "@/types/error";
+import { useState } from "react";
+
+export default function useValidationCheck(testFn: () => Validation) {
+  const [errorMessage, setErrorMessage] = useState("");
+  function checkValidation() {
+    const testResult = testFn();
+    if (!testResult.result && testResult.errorMsg) {
+      setErrorMessage(testResult?.errorMsg);
+      return false;
+    }
+    return true;
+  }
+  return { errorMessage, checkValidation };
+}
