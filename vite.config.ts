990180f1f4bf0de22d2b3eb6503aa50690635581
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 01:10:18 +0900

    chore : 서버 cors 에러 해결을 위한 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://3.38.8.19.nip.io/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
