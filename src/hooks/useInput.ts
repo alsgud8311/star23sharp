d2dcc3cb9be8ea05ed5a2eea9162e859027917d9
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 01:20:48 +0900

    refactor : input change event에 에러 검증할 수 있도록 추가

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -6,11 +6,21 @@ export type MessageInputHook = {
     e: ChangeEvent<HTMLTextAreaElement> | ChangeEvent<HTMLInputElement>,
   ) => void;
 };
-export default function useInput(initialText?: string) {
+export default function useInput(
+  initialText?: string,
+  test?: (val: string) => boolean,
+) {
   const [text, setText] = useState(initialText || "");
   function updateText(
     e: ChangeEvent<HTMLTextAreaElement> | ChangeEvent<HTMLInputElement>,
   ) {
+    if (test) {
+      if (test(e.target.value)) {
+        setText(e.target.value);
+        return;
+      }
+      return;
+    }
     setText(e.target.value);
   }
   return {
