396aedf83ccff1ad0a1b82bc7ec79e5cb61ec0b8
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 02:24:27 +0900

    refactor : 버튼 컴포넌트 disabled 프로퍼티 추가

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -4,17 +4,20 @@ export default function Button({
   children,
   type = "button",
   onClick,
+  disabled,
   ...props
 }: {
   children: ReactNode;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
+  disabled?: boolean;
 }) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="border-2 border-black py-3 px-8 rounded-xl cursor-pointer hover:bg-slate-200 transform duration-300"
+      className="transform cursor-pointer rounded-xl border-2 border-black px-8 py-3 duration-300 hover:bg-slate-200"
+      disabled={disabled ?? false}
       {...props}
     >
       {children}
