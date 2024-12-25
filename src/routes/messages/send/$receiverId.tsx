880d97d7814e6050b316ff4974743c647221d68d
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 18:51:02 +0900

    배포테스트 12

--- src/routes/messages/send/$receiverId.tsx
+++ src/routes/messages/send/$receiverId.tsx
@@ -53,58 +53,56 @@ function MessageSendComponent() {
         <header className="flex w-full items-center justify-center border-b-2 border-black p-3 text-xl">
           메시지
         </header>
-        <div className="flex flex-1 flex-col overflow-hidden">
-          <div className="relative h-full w-full">
-            <div
-              className="absolute inset-0 h-full w-full touch-pan-y flex-col gap-2 overflow-y-auto p-4"
-              id="messageSend"
-              style={{ WebkitOverflowScrolling: "touch" }}
-            >
-              <div className="mb-4 flex items-center justify-center">
-                <Button onClick={openCheckMessageModal}>
-                  <div className="flex w-full items-center justify-center gap-2">
-                    <img src={messageIcon} alt="message" className="w-5" />내
-                    메시지 확인하기
-                  </div>
-                </Button>
-              </div>
-              <textarea
-                className="h-72 w-full break-all border-2 border-black p-2 text-lg outline-none"
-                placeholder="친구에게 하고 싶었지만 제대로 하지 못했던 말을 적어보세요"
-                onChange={updateBody}
-                maxLength={500}
-                value={body}
+        <div className="relative flex flex-1 flex-col overflow-hidden">
+          <div
+            className="absolute inset-0 h-full w-full touch-pan-y flex-col gap-2 overflow-y-scroll p-4"
+            id="messageSend"
+            style={{ WebkitOverflowScrolling: "touch" }}
+          >
+            <div className="mb-4 flex items-center justify-center">
+              <Button onClick={openCheckMessageModal}>
+                <div className="flex w-full items-center justify-center gap-2">
+                  <img src={messageIcon} alt="message" className="w-5" />내
+                  메시지 확인하기
+                </div>
+              </Button>
+            </div>
+            <textarea
+              className="h-72 w-full break-all border-2 border-black p-2 text-lg outline-none"
+              placeholder="친구에게 하고 싶었지만 제대로 하지 못했던 말을 적어보세요"
+              onChange={updateBody}
+              maxLength={500}
+              value={body}
+            />
+            <div className="flex flex-col text-lg">
+              <p>보내는사람</p>
+              <input
+                type="text"
+                value={sender}
+                maxLength={15}
+                onChange={updateSender}
+                className="w-3/4 border-b border-black outline-none"
               />
-              <div className="flex flex-col text-lg">
-                <p>보내는사람</p>
-                <input
-                  type="text"
-                  value={sender}
-                  maxLength={15}
-                  onChange={updateSender}
-                  className="w-3/4 border-b border-black outline-none"
-                />
-              </div>
-              <div className="text-lg">
-                <p>받는사람</p>
-                <p className="text-sm">{room.title}</p>
-              </div>
+            </div>
+            <div className="text-lg">
+              <p>받는사람</p>
+              <p className="text-sm">{room.title}</p>
             </div>
           </div>
         </div>
-        <footer className="flex h-10 w-full items-center justify-between border-y-2 border-black text-xl">
-          <button className="w-1/3" onClick={() => navigate({ to: "/" })}>
-            홈
-          </button>
-          <button
-            className="w-1/3"
-            onClick={() => send({ room_signature, sender, body })}
-            disabled={isPending}
-          >
-            보내기
-          </button>
-        </footer>
       </div>
+      <footer className="flex h-10 w-full items-center justify-between border-y-2 border-black text-xl">
+        <button className="w-1/3" onClick={() => navigate({ to: "/" })}>
+          홈
+        </button>
+        <button
+          className="w-1/3"
+          onClick={() => send({ room_signature, sender, body })}
+          disabled={isPending}
+        >
+          보내기
+        </button>
+      </footer>
       <CheckMessageModal
         modal={modal}
         openModal={openModal}
