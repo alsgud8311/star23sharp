9a817a3badded03dc4a7847444b9505fc4df579b
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 13:40:47 +0900

    refactor : 메인페이지 구조 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,31 @@
-import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import postbox from '@assets/postbox.png'
-import Button from '@/components/common/button'
-import React from 'react'
-import { useRoomStore } from '@/store/useRoomStore'
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import postbox from "@assets/postbox.png";
+import Button from "@/components/common/button";
+import React from "react";
+import { useRoomStore } from "@/store/useRoomStore";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  const navigate = useNavigate()
-  const loggedIn = useRoomStore((state) => state.messageRoom)
+  const navigate = useNavigate();
+  const loggedIn = useRoomStore((state) => state.messageRoom);
   return (
     <React.Fragment>
       <img
         src={postbox}
         alt="별이삼샵"
-        className="w-1/2 translate-y-10 transform animate-slide-up opacity-0"
+        className="mb-3 w-2/5 translate-y-10 transform animate-slide-up opacity-0"
       />
-      <p className="p-4 text-xl">별이삼샵</p>
+      <div className="mb-5 text-center">
+        <p className="text-xl">별이삼샵</p>
+        <p>나만의 프라이빗 롤링페이퍼</p>
+      </div>
       <div className="flex flex-col gap-3">
         <Button
           onClick={() => {
-            navigate({ to: '/create' })
+            navigate({ to: "/create" });
           }}
         >
           시작하기
@@ -30,7 +33,7 @@ function RouteComponent() {
         {loggedIn && (
           <Button
             onClick={() => {
-              navigate({ to: '/messages' })
+              navigate({ to: "/messages" });
             }}
           >
             내 메시지함
@@ -38,5 +41,5 @@ function RouteComponent() {
         )}
       </div>
     </React.Fragment>
-  )
+  );
 }
