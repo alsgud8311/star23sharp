e4c90d607368a5207c149feb26fca14be2cce61b
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 13:38:13 +0900

    chore : notfound 안내 텍스트 변경

--- src/components/common/notFound.tsx
+++ src/components/common/notFound.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     <div className="flex h-full w-full flex-col items-center justify-center gap-10 bg-white text-black">
       <img src={oops} className="w-1/3" alt="oops" />
       <div className="flex flex-col gap-3">
-        <p>존재하지 않는 링크예요</p>
+        <p>유효하지 않은 링크예요</p>
         <Button onClick={() => navigate({ to: "/" })}>홈으로</Button>
       </div>
     </div>
