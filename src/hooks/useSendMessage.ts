39c58444126d5d211707c2dbc1e26efd1ce9751d
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 13:40:09 +0900

    feat : 메세지 보내기 에러처리

--- src/hooks/useSendMessage.ts
+++ src/hooks/useSendMessage.ts
@@ -1,13 +1,23 @@
 import { sendMessage, SendMessageRequest } from "@/api/message.api";
+import { useErrorStore } from "@/store/useErrorStore";
 import { useMutation } from "@tanstack/react-query";
 
 export default function useSendMessage() {
+  const pushError = useErrorStore((state) => state.pushError);
   const { mutate, isPending, isError, isSuccess } = useMutation({
     mutationFn: (sendMessageRequest: SendMessageRequest) =>
       sendMessage(sendMessageRequest),
   });
 
   function send(sendMessageRequest: SendMessageRequest) {
+    if (!sendMessageRequest.body) {
+      pushError("내용을 입력해주세요!");
+      return;
+    }
+    if (!sendMessageRequest.sender) {
+      pushError("보내는사람을 입력해주세요!");
+      return;
+    }
     mutate(sendMessageRequest);
   }
 
