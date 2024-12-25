4e5f7eb214dfac9b106e3f575175d59ca8aa73f3
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 16:11:38 +0900

    fix : 모바일 디바이스 내 높이 조정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { createRootRoute } from "@tanstack/react-router";
 import Phone from "../components/phone";
+import resize from "@/utils/resize";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
+  resize();
   return <Phone></Phone>;
 }
---
+++ src/utils/resize.ts
@@ -0,0 +1,10 @@
+export default function resize() {
+  const vh = window.innerHeight * 0.01;
+
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  window.addEventListener("resize", () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+}
