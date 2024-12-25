d18cee9df88c0df73f064c0f60e86a747982f830
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 13:39:27 +0900

    style : 로딩 모달 스타일 변경

--- src/components/modals/loadingModal.tsx
+++ src/components/modals/loadingModal.tsx
@@ -1,15 +1,21 @@
-import PushModal from "@/components/common/pushModal";
 import { useLoadingStore } from "@/store/useLoadingStore";
 import loadingSpinner from "@assets/loading.gif";
 
 export default function LoadingModal() {
   const loading = useLoadingStore((state) => state.isLoading);
   return (
-    <PushModal modal={loading}>
-      <div className="flex w-full flex-col items-center justify-center gap-5">
-        <img src={loadingSpinner} alt="loading" className="w-1/3" />
-        <p>로딩중...</p>
-      </div>
-    </PushModal>
+    <>
+      {loading && (
+        <>
+          <div className="absolute left-0 top-0 z-40 h-full w-full bg-black opacity-30" />
+          <div className="absolute left-0 top-0 z-40 flex h-full w-full items-center justify-center">
+            <div className="z-50 flex w-full flex-col items-center justify-center gap-5">
+              <img src={loadingSpinner} alt="loading" className="w-1/3" />
+              <p className="text-xl">로딩중...</p>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
