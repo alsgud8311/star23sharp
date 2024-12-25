47dd5b09cc38e3a23629eb43ce2daf9345bc6459
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-25 13:41:05 +0900

    feat : 삭제 확인 모달 추가

--- src/routes/messages/$messageId/index.tsx
+++ src/routes/messages/$messageId/index.tsx
@@ -1,5 +1,7 @@
 import { getMessageDetail } from "@/api/message.api";
 import NotFound from "@/components/common/notFound";
+import ConfirmModal from "@/components/modals/confirmModal";
+import useDeleteMessage from "@/hooks/useDeleteMessage";
 import { Message } from "@/types/message";
 import {
   createFileRoute,
@@ -25,37 +27,50 @@ export const Route = createFileRoute("/messages/$messageId/")({
 function DeatilMessageComponent() {
   const detailData: Message = useLoaderData({ from: "/messages/$messageId/" });
   const navigate = useNavigate();
+  const { modal, openModal, closeModal, deleteInform, messageDelete } =
+    useDeleteMessage(detailData.id.toString());
   return (
-    <div className="flex h-full w-full flex-col">
-      <header className="flex w-full items-center justify-center border-b-2 border-black p-3 text-xl">
-        메시지
-      </header>
-      <div className="flex flex-grow flex-col gap-2 p-4">
-        <div className="h-40 w-full overflow-y-scroll break-all border-2 border-black p-2 text-xl min-[380px]:h-80">
-          {detailData.body}
+    <>
+      <div className="flex h-full w-full flex-col">
+        <header className="flex w-full items-center justify-center border-b-2 border-black p-3 text-xl">
+          메시지
+        </header>
+        <div className="flex flex-grow flex-col gap-2 p-4">
+          <div className="h-40 w-full overflow-y-scroll break-all border-2 border-black p-2 text-xl min-[380px]:h-80">
+            {detailData.body}
+          </div>
+          <div>
+            <p>보낸사람</p>
+            <p>
+              {detailData.sender === "*23#"
+                ? "발신자표시제한"
+                : detailData.sender}
+            </p>
+          </div>
+          <div>
+            <p>보낸시간</p>
+            <p>{detailData.created_at}</p>
+          </div>
         </div>
-        <div>
-          <p>보낸사람</p>
-          <p>
-            {detailData.sender === "*23#"
-              ? "발신자표시제한"
-              : detailData.sender}
-          </p>
-        </div>
-        <div>
-          <p>보낸시간</p>
-          <p>24/12/24 12:22</p>
-        </div>
+        <footer className="flex h-10 w-full items-center justify-between border-y-2 border-black text-xl">
+          <button className="w-1/3" onClick={() => navigate({ to: "/" })}>
+            홈
+          </button>
+          <button className="w-1/3" onClick={openModal}>
+            삭제
+          </button>
+          <button className="w-1/3" onClick={() => navigate({ to: ".." })}>
+            뒤로
+          </button>
+        </footer>
       </div>
-      <footer className="flex h-10 w-full items-center justify-between border-y-2 border-black text-xl">
-        <button className="w-1/3" onClick={() => navigate({ to: "/" })}>
-          홈
-        </button>
-        <button className="w-1/3">확인</button>
-        <button className="w-1/3" onClick={() => navigate({ to: ".." })}>
-          뒤로
-        </button>
-      </footer>
-    </div>
+      <ConfirmModal
+        modal={modal}
+        openModal={openModal}
+        closeModal={closeModal}
+        inform={deleteInform}
+        onConfirm={messageDelete}
+      />
+    </>
   );
 }
